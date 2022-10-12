c8e80a7bde2442676478527fd8a6ded96e2cd816
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-12 13:56:43 +0200

    Exercise concluded.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const Alert = () => {
   const colorClasses = {
-    color: "red",
-	text4Red:"OMG! Something really bad has happended!",
-	text4Orange:"Well, it is not that bad after all!"
+    orange: "", // comment this, uncomment next
+    // orange: "warning",
+    red: "danger",
+    text4Red: "OMG! Something really bad has happended!",
+    text4Orange: "Well, it is not that bad after all!",
   };
-  //your component here
 
+  return (
+    <div
+      class={`alert alert-${colorClasses.orange ? "warning" : "danger"}`}
+      role="alert"
+    >
+      {colorClasses.orange ? colorClasses.text4Orange : colorClasses.text4Red}
+    </div>
+  );
 };
 
-// here is where the alert component is being used, you don't have to edit this part,
-// but it helps you understand what properties are being passed to the component
-ReactDOM.render(
-  <Alert/>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Alert />, document.getElementById("root"));
